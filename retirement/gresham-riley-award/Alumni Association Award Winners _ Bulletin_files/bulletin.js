ebb3f0117e4debdada0d8da659f427f8deba5b9b
Scott Veirs <scott@beamreach.org>
2022-11-25 14:10:00 -0800

    Add all local directory changes

--- retirement/gresham-riley-award/Alumni Association Award Winners _ Bulletin_files/bulletin.js
+++
@@ -1,8 +0,0 @@
-var $ = jQuery.noConflict();
-$(document).ready(function(event){
-	// add  zero-width space &#8203; to our looooong @coloradocollege email addresses  so they can wrap if needed
-	$('a[href^="mailto:"]:contains("coloradocollege")' ).each(function(){
-		var lnktxt = $(this).html().replace(/coloradocollege/gi, "colorado&#8203;college");
-		$(this).html(lnktxt);
-	});
-});
